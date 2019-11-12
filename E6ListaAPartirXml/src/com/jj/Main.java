413e8e5aa17a0cfc635636a6e2b571461c8da625
JJ <jjaimes20@gmail.com>
2019-11-12 17:56:46 +0100

    ejercicio acabado

--- E6ListaAPartirXml/src/com/jj/Main.java
+++ E6ListaAPartirXml/src/com/jj/Main.java
@@ -1,5 +1,6 @@
 package com.jj;
 
+import com.sun.net.httpserver.Headers;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -8,6 +9,10 @@ import org.w3c.dom.NodeList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static javax.swing.text.html.CSS.getAttribute;
 
 public class Main {
 
@@ -21,25 +26,36 @@ public class Main {
             document.getDocumentElement().normalize();
 
             System.out.println("Elemento raíz: " + document.getDocumentElement().getNodeName());
-            NodeList productos = document.getElementsByTagName("empleado");
+            NodeList productos = document.getElementsByTagName("element");
+            //
+
+            ArrayList<String> descripciones = new ArrayList<>();
 
             for (int i = 0; i < productos.getLength(); i ++) {
 
-                Node emple = productos.item(i);
+                Node nNode = productos.item(i);
 
-                if (emple.getNodeType() == Node.ELEMENT_NODE) {
+                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 
-                    Element elemento = (Element) emple;
-                    /*
-                    System.out.println("ID: " + getNodo("id", elemento));
-                    System.out.println("Apellido: " + getNodo("apellido", elemento));
-                    System.out.println("Departamento: " + getNodo("dep", elemento));
-                    System.out.println("Salario: " + getNodo("salario", elemento));
-                    */
-
-                }
+                     Element elemento = (Element) nNode;
+                     System.out.println( i+1 + ") " + elemento.getAttribute("header"));
+                     descripciones.add(elemento.getAttribute("text"));
+                 }
             }
-        } catch (Exception e) { e.printStackTrace();}
+            System.out.println("Introdueix el número del curs del que vols informació: ");
 
+            Scanner lector = new Scanner(System.in);
+            int posicionDesc = Integer.valueOf(lector.nextLine())-1;
+            System.out.println("Informació del curs:");
+            if (descripciones.get(posicionDesc).equals(""))
+            {
+                System.out.println("no tiene descripcion");
+            }
+            else
+            {
+                System.out.println(descripciones.get(posicionDesc));
+            }
+
+        } catch (Exception e) { e.printStackTrace();}
     }
 }
